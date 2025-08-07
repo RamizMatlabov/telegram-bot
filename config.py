39053036aa52cdaf5e0491d37b7ccca427302d2a
Ramiz <ramizmatlabov923@gmail.com>
2025-08-07 15:29:47 +0500

    commit

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class BotConfig:
     admin_id: int  # Your Telegram user ID
 
 # Get your token from environment variable or put it directly
-BOT_TOKEN = os.getenv('BOT_TOKEN', 'YOUR_BOT_TOKEN_HERE')
+BOT_TOKEN = os.getenv('BOT_TOKEN', '8207927039:AAFbPl7ZbihfZSJjV2lG3csQ_M7FTna_0Yg')
 ADMIN_ID = int(os.getenv('ADMIN_ID', '123456789'))  # Replace with your Telegram ID
 
 config = BotConfig(
